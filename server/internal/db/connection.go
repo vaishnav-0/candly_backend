2fd8068c48ba96134bdb4a8fd1fcfab76f7db3a0
Vaishnav <vaishnavvaichu123@gmail.com>
2023-03-06 11:53:49 +0530

    changed to pool

--- server/internal/db/connection.go
+++ server/internal/db/connection.go
@@ -2,10 +2,11 @@ package db
 
 import (
 	"context"
-	"github.com/jackc/pgx/v5"
 	"net/url"
+	"github.com/jackc/pgx/v5/pgxpool"
+
 )
 
-func Open(host, username, password, database string) (*pgx.Conn, error) {
-	return pgx.Connect(context.Background(), "postgresql://"+username+":"+url.QueryEscape(password)+"@"+host+"/"+database)
+func Open(host, username, password, database string) (*pgxpool.Pool, error) {
+	return pgxpool.New(context.Background(), "postgresql://"+username+":"+url.QueryEscape(password)+"@"+host+"/"+database)
 }
