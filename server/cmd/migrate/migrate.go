f5b2aa8dd77ed1a15be18f8acf781d7f576b78c7
Vaishnav <vaishnavvaichu123@gmail.com>
2023-07-31 18:16:55 +0530

    migration script

--- server/cmd/migrate/migrate.go
+++ server/cmd/migrate/migrate.go
@@ -3,15 +3,35 @@ package main
 import (
 	"candly/internal/config"
 	"fmt"
+	"github.com/akamensky/argparse"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+
 	"net/url"
 	"os"
-	"strconv"
 )
 
 func main() {
+
+	parser := argparse.NewParser("migrate", "go migrate for candly")
+
+	verCmd := parser.NewCommand("v", "Get current version and dirty flag")
+
+	downCmd := parser.NewCommand("down", "migrate all the way down")
+	upCmd := parser.NewCommand("up", "migrate all the way up")
+
+	stepCmd := parser.NewCommand("step", "step to version")
+	ver := stepCmd.IntPositional(&argparse.Options{Help: "version", Required: true})
+
+	forceCmd := parser.NewCommand("force", "force to version")
+	fVer := forceCmd.IntPositional(&argparse.Options{Help: "version", Required: true})
+
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+	}
+
 	c := config.GetConfig()
 
 	m, err := migrate.New(
@@ -21,47 +41,37 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	if len(os.Args) == 3 {
-		args := os.Args[1:]
-		version, err := strconv.Atoi(args[1])
+
+	if forceCmd.Happened() {
+
+		if err = m.Force(*fVer); err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to force: %v\n", err)
+			os.Exit(1)
+		}
+		return
+	} else if upCmd.Happened() {
+
+		err = m.Up()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to up: %v\n", err)
+			os.Exit(1)
+		}
+	} else if stepCmd.Happened() {
+		err = m.Steps(*ver)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to up: %v\n", err)
+			os.Exit(1)
+		}
+	} else if downCmd.Happened() {
+
+		err = m.Down()
 
 		if err != nil {
-			fmt.Println("Invalid number")
-			return
+			fmt.Fprintf(os.Stderr, "Unable to down: %v\n", err)
+			os.Exit(1)
 		}
-		if args[0] == "-f" {
-
-			if err = m.Force(version); err != nil {
-				fmt.Fprintf(os.Stderr, "Unable to force: %v\n", err)
-				os.Exit(1)
-			}
-			return
-		}
-
-		if args[0] == "-u" {
-			if version == -1 {
-				err = m.Up()
-			} else {
-				err = m.Steps(version)
-			}
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Unable to up: %v\n", err)
-				os.Exit(1)
-			}
-			return
-		}
-		if args[0] == "-d" {
-			if version == -1 {
-				err = m.Down()
-			} else {
-				err = m.Steps(-version)
-			}
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Unable to down: %v\n", err)
-				os.Exit(1)
-			}
-			return
-		}
+	} else if verCmd.Happened() {
+		fmt.Println(m.Version())
 	}
 
 }
--- server/go.mod
+++ server/go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/akamensky/argparse v1.4.0 // indirect
 	github.com/bahlo/generic-list-go v0.2.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
@@ -41,11 +42,13 @@ require (
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.0 // indirect
+	github.com/jonhoo/go-events v0.0.0-20150121182257-5b240ffae643 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/jpillora/cookieauth v1.1.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
